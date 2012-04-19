c232631597a110466accb996406d2b90f603546d
Jozef Hartinger <jharting@redhat.com>
2012-04-19 13:16:48 +0200

    Just a test

--- impl/src/main/java/org/jboss/weld/executor/FixedThreadPoolExecutorServices.java
+++ impl/src/main/java/org/jboss/weld/executor/FixedThreadPoolExecutorServices.java
@@ -64,7 +64,7 @@ public class FixedThreadPoolExecutorServices extends AbstractExecutorServices {
     private final ExecutorService executor;
 
     public FixedThreadPoolExecutorServices() {
-        this(16);
+        this(1);
     }
 
     public FixedThreadPoolExecutorServices(int threadPoolSize) {
